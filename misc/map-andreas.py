947c47a6a6b72b817f4e1ef84d05632f0aab0f9c
Southclaw <southclawjk@gmail.com>
2016-03-24 16:28:00 +0000

    Fixed some item offsets and changed metal models

---
+++ misc/map-andreas.py
@@ -0,0 +1,17 @@
+import os
+import io
+
+
+HMAP_PATH = "..\\scriptfiles\\SAfull.hmap"
+
+POINT_DATA = []
+
+
+def load_hmap():
+	with io.open(HMAP_PATH, "rb") as f:
+		f.read
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
