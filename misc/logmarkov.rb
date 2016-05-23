56873bfcc4b618013ae1f66a625d67b589ab372d
Southclaw <southclawjk@gmail.com>
2016-05-23 11:12:12 +0100

    Updated logmarkov.rb to properly parse log files
    Extracts all [CHAT] lines whether they are LOCAL, GLOBAL or RADIO (or
    the older radio format that included the frequency which had to be
    removed due to an unnamed VPS owner peeking at messages stealing radio
    frequencies!). Also strips out one-word messages and text-tags.

--- misc/logmarkov.rb
+++ misc/logmarkov.rb
@@ -4,9 +4,8 @@ require 'twitter'
 
 
 # read settings
-file = File.read('config.json')
+file = File.read('logmarkov.cfg')
 data = JSON.parse(file)
-
 
 CONSUMER_KEY = data["consumer_key"]
 CONSUMER_SECRET = data["consumer_secret"]
@@ -14,10 +13,14 @@ ACCESS_TOKEN = data["access_token"]
 ACCESS_SECRET = data["access_secret"]
 LOG_DIRECTORY = data["log_directory"]
 
+# compile regex for chat and things to remove
+# \[CHAT\] \[(GLOBAL|LOCAL|RADIO|[0-9\.]*)\] \[[a-zA-Z0-9\(\)\[\]\._$=@]*\]: ([a-zA-Z0-9 !"#$%&'()*+,\-./:;<=>?@[\\\]^_`|~]*)
+chat = Regexp.new(/\[CHAT\] \[(GLOBAL|LOCAL|RADIO|[0-9\.]*)\] \[[a-zA-Z0-9\(\)\[\]\._$=@]*\]: ([a-zA-Z0-9 !\"#$%&\'()*+,\-.\/:;<=>?@^_`|~]*)/)
+colc = Regexp.new(/\{[0-9a-fA-F]{1,8}\}/)
+tags = Regexp.new(/(@[0-9])|(&[a-z])/)
 
 # open dict
 markov = MarkyMarkov::Dictionary.new('logdict')
-
 
 if ARGV[0] == "build"
 	Dir.foreach(LOG_DIRECTORY) do |item|
@@ -25,29 +28,47 @@ if ARGV[0] == "build"
 		next if item == '.' or item == '..'
 
 		puts "Parsing file " + item + "..."
-		#todo extract [chat] only with grep or something
-		markov.parse_file LOG_DIRECTORY + item
+		File.foreach(LOG_DIRECTORY + item) do |line|
+
+			c = line.match(chat)
+
+			if c != nil and c.length > 2
+				final_string = c[2].gsub(tags, "").strip
+				if final_string.split.size > 1
+					#puts final_string
+					markov.parse_string final_string
+				end
+			end
+
+		end
+
 		puts "Parsing complete!"
 
 	end
 	markov.save_dictionary!
-end
 
+else
+	# post a tweet
 
-# post a tweet
+	s = markov.generate_1_sentence
+	puts s
 
-client = Twitter::REST::Client.new do |config|
-	config.consumer_key        = CONSUMER_KEY
-	config.consumer_secret     = CONSUMER_SECRET
-	config.access_token        = ACCESS_TOKEN
-	config.access_token_secret = ACCESS_SECRET
-end
+	puts "post?"
+	answer = gets
 
-s = markov.generate_1_sentence
-puts s
+	if answer == "y\n"
+		puts "Posting to twitter!"
+		client = Twitter::REST::Client.new do |config|
+			config.consumer_key        = CONSUMER_KEY
+			config.consumer_secret     = CONSUMER_SECRET
+			config.access_token        = ACCESS_TOKEN
+			config.access_token_secret = ACCESS_SECRET
+		end
 
-begin
-	client.update(s)
-rescue => error
-	puts error.message
+		begin
+			client.update(s)
+		rescue => error
+			puts error.message
+		end
+	end
 end
