df9b36fbe7f269d4ffa7be7621dfb8435996111d
Southclaw <southclawjk@gmail.com>
2016-07-25 15:08:42 +0100

    Updated fetch-compile-run script to support branches, Added build-config autocreation

--- fetch-compile-run.py
+++ fetch-compile-run.py
@@ -1,24 +1,49 @@
+import platform
 import io
 import json
 import subprocess
 
 
-COMPILER_PATH = "../pawno/pawncc.exe"
+config = {}
 
-ret = 0
-ret += subprocess.call(["git", "fetch"])
-ret += subprocess.call(["git", "merge"])
 
-with io.open("build-config.json") as f:
-	config = json.load(f)
+try:
+	with io.open("build-config.json", 'r') as f:
+		config = json.load(f)
+
+except IOError as e:
+	print(e)
+	print("Creating build-config with defaults...")
+
+	if platform.system() == "Windows":
+		config["compiler_path"] = "../pawno/pawncc.exe"
+
+	else:
+		config["compiler_path"] = "../pawnc-3.10.20160702-linux/bin/pawncc"
+
+	config["branch"] = "master"
+
+	with io.open("build-config.json", 'w') as f:
+		json.dump(config, f, indent=4)
+
+	print(config["compiler_path"], config["branch"])
+
 
 COMPILER_PATH = config["compiler_path"]
+BRANCH = config["branch"]
+
+
+ret = 0
+ret += subprocess.call(["git", "checkout", BRANCH])
+ret += subprocess.call(["git", "fetch"])
+ret += subprocess.call(["git", "merge"])
 
 if ret > 0:
 	print("git error")
 
+
 while True:
-	ret = subprocess.call([COMPILER_PATH, "-Dgamemodes/", "ScavengeSurvive.pwn", "-;+", "-(+", "-\\)+", "-d3",])
+	ret = subprocess.call([COMPILER_PATH, "-Dgamemodes/", "ScavengeSurvive.pwn", "-;+", "-(+", "-\\)+", "-d3"])
 	if ret > 0:
 		print("compilation error")
 		break
