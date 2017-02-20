859b00c1317d40bc27adf53f5f044e01af682e05
Southclaw <southclawjk@gmail.com>
2017-02-20 09:06:50 +0000

    implemented database access, added better filenames

---
+++ goss/goss_db_main.go
@@ -0,0 +1,20 @@
+package ssdb
+
+import (
+	"github.com/jinzhu/gorm"
+	// gorm sqlite dialect
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+// ConnectDatabase sets up database connections
+func ConnectDatabase(location string, logmode bool) (*gorm.DB, error) {
+	db, err := gorm.Open("sqlite3", location)
+	if err != nil {
+		return nil, err
+	}
+
+	db.LogMode(logmode)
+	db.AutoMigrate(&PlayerAccount{})
+
+	return db, nil
+}
