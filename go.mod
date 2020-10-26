66477b0de21bf164e6c32712049f6a171c9874e1
Southclaws <accounts@southcla.ws>
2020-10-26 19:52:15 +0000

    switched to discordgo for stability

--- go.mod
+++ go.mod
@@ -5,11 +5,13 @@ go 1.14
 require (
 	github.com/Southclaws/go-samp-query v1.1.2
 	github.com/Southclaws/sampctl v0.0.0-20201015215605-f6e02560637f
+	github.com/bwmarrin/discordgo v0.22.0
 	github.com/cskr/pubsub v1.0.2
-	github.com/diamondburned/arikawa v1.3.7
 	github.com/fsnotify/fsnotify v1.4.9
 	github.com/go-chi/chi v4.1.2+incompatible
+	github.com/google/go-cmp v0.5.2 // indirect
 	github.com/google/go-github v0.0.0-20180819205025-d7732128a00e
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/joho/godotenv v1.3.0
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/kr/pretty v0.2.0
@@ -17,7 +19,9 @@ require (
 	github.com/stretchr/testify v1.5.1
 	go.uber.org/atomic v1.6.0
 	go.uber.org/zap v1.16.0
-	google.golang.org/api v0.33.0
+	golang.org/x/oauth2 v0.0.0-20200902213428-5d25da1a8d43 // indirect
+	golang.org/x/sys v0.0.0-20200905004654-be1d3432aa8f // indirect
+	golang.org/x/tools v0.0.0-20200904185747-39188db58858 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 	gopkg.in/src-d/go-git.v4 v4.13.1 // indirect
 )
--- runner/discord.go
+++ runner/discord.go
@@ -6,63 +6,49 @@ import (
 	"time"
 
 	sampquery "github.com/Southclaws/go-samp-query"
+	"github.com/bwmarrin/discordgo"
 	"github.com/cskr/pubsub"
-	"github.com/diamondburned/arikawa/discord"
-	"github.com/diamondburned/arikawa/gateway"
-	"github.com/diamondburned/arikawa/session"
 	"go.uber.org/zap"
 )
 
 func RunDiscord(ctx context.Context, ps *pubsub.PubSub, cfg Config) {
-	s, err := session.New("Bot " + cfg.DiscordToken)
+	discord, err := discordgo.New("Bot " + cfg.DiscordToken)
 	if err != nil {
 		panic(err)
 	}
 
-	channelid := new(discord.ChannelID)
-	if err := channelid.UnmarshalJSON([]byte(cfg.DiscordChannel)); err != nil {
-		panic(err)
-	}
+	discord.ChannelMessageSend(cfg.DiscordChannel, "Scavenge and Survive server starting!")
 
-	s.AddHandler(func(c *gateway.MessageCreateEvent) {
-		switch c.Content {
+	discord.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		switch m.Message.Content {
 		case "/status":
 			server, err := sampquery.GetServerInfo(ctx, "play.scavengesurvive.com:7777", false)
 			if err != nil {
-				s.SendMessage(*channelid, "Failed to query :("+err.Error(), nil)
+				discord.ChannelMessageSend(cfg.DiscordChannel, "Failed to query :("+err.Error())
 			} else {
-				s.SendMessage(*channelid, "", &discord.Embed{
+				discord.ChannelMessageSendEmbed(cfg.DiscordChannel, &discordgo.MessageEmbed{
 					Title: "Server Status",
-					Type:  discord.NormalEmbed,
+					Type:  discordgo.EmbedTypeRich,
 					Description: fmt.Sprintf(
 						"Players: %d/%d\nPing: %d",
 						server.Players,
 						server.MaxPlayers,
 						server.Ping,
 					),
-					Color: discord.Color(0xff4700),
+					Color: 0xff4700,
 				})
 			}
 		}
 	})
 
-	if err := s.Open(); err != nil {
-		panic(err)
-	}
-	defer s.Close()
-
-	if _, err = s.Me(); err != nil {
-		panic(err)
-	}
-
 	for range ps.Sub("server_restart") {
-		if _, err := s.SendMessage(*channelid, "Server restart!", nil); err != nil {
+		if _, err := discord.ChannelMessageSend(cfg.DiscordChannel, "Server restart!"); err != nil {
 			zap.L().Error("failed to send discord message", zap.Error(err))
 		}
 	}
 
 	for d := range ps.Sub("server_update") {
-		if _, err := s.SendMessage(*channelid, fmt.Sprintf("A server update is on the way in %s", d.(time.Duration)), nil); err != nil {
+		if _, err := discord.ChannelMessageSend(cfg.DiscordChannel, fmt.Sprintf("A server update is on the way in %s", d.(time.Duration))); err != nil {
 			zap.L().Error("failed to send discord message", zap.Error(err))
 		}
 	}
