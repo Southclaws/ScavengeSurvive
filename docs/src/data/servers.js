27d8cbe378a51e406837b4a64ff2cc2facb37046
Flávio Pereira <me@flaviopereira.dev>
2024-06-22 17:13:46 +0100

    fix(docs/servers) wrong discord link

--- docs/src/data/servers.js
+++ docs/src/data/servers.js
@@ -9,7 +9,7 @@ const servers = [
     address : '200.163.145.186:7777',
     name    : 'Scavenge And Survive - Apocalypse',
     language: 'Portuguese',
-    links   : { discord: 'https://discord.gg/AEpYnCZX' }
+    links   : { discord: 'https://discord.gg/s3kmS5Jn9D' }
   }
 ];
 
