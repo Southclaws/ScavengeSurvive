147d771c8b882c799ce124dfdcaace5d048b2d90
Pepper94 <rumenkostoff@gmail.com>
2024-12-06 15:04:33 +0200

    Update servers.js

--- docs/src/data/servers.js
+++ docs/src/data/servers.js
@@ -1,6 +1,6 @@
 const servers = [
   {
-    address : '178.63.13.143:18625',
+    address : '178.63.13.143:19025',
     name    : 'Scavenge and Survive - New Age!',
     language: 'English',
     links   : { discord: 'https://discord.com/invite/vrkEMqXsQ2' }
