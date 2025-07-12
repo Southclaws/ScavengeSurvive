399ba8e8fd164243b32fb694f4bb6ce37fe40c35
Flávio Pereira <flavioaspereira@gmail.com>
2025-07-12 13:31:08 +0100

    server listing update (#649)
    * rm apocalypse server
    
    * updated new age sv ip

--- docs/src/data/servers.js
+++ docs/src/data/servers.js
@@ -1,15 +1,9 @@
 const servers = [
   {
-    address : '178.63.13.143:19025',
+    address : '135.125.128.240:7897',
     name    : 'Scavenge and Survive - New Age!',
     language: 'English',
     links   : { discord: 'https://discord.com/invite/vrkEMqXsQ2' }
-  },
-  {
-    address : '188.165.192.24:7556',
-    name    : 'Scavenge And Survive - Apocalypse',
-    language: '',
-    links   : { discord: 'https://discord.gg/s3kmS5Jn9D' }
   }
 ];
 
